357551fc69143b17ce156cb8706b8ef551ec1a36
Raj singhania <88094082+rajwithcodes@users.noreply.github.com>
2025-08-14 14:22:14 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review.js');
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -21,7 +22,16 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
-
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: { $in: listing.reviews } }); // Delete all reviews associated with the listing
+        
+    }
+})
 const Listing = mongoose.model('Listing', listingSchema);
 module.exports = Listing;
